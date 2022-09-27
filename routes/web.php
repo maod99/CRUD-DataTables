783269f744c69138e8a867c5d4a9d2261551f8f9
maod99 <maodgarad@gamil.com>
2022-09-28 01:20:23 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\DataTableAjaxCRUDController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('ajax-crud-datatable', [DataTableAjaxCRUDController::class, 'index']);
+Route::post('store-company', [DataTableAjaxCRUDController::class, 'store']);
+Route::post('edit-company', [DataTableAjaxCRUDController::class, 'edit']);
+Route::post('delete-company', [DataTableAjaxCRUDController::class, 'destroy']);
